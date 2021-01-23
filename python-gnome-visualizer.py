b7cc77061c8f6c7284aa98bc745ccfcf35d4cd12
Gagan Bhat <gaganbhat6@gmail.com>
2021-01-23 15:08:10 -0600

    Updated gif quality and sizing

--- python-gnome-visualizer.py
+++ python-gnome-visualizer.py
@@ -35,12 +35,12 @@ duration = 5
 def make_frame(t):
     top_nucleotide_index = t * graphic_record.sequence_length / duration
     graphic_record.top_position = top_nucleotide_index
-    ax, _ = graphic_record.plot(figure_width=8, figure_height=11)
+    ax, _ = graphic_record.plot(figure_width=8, figure_height=8)
     ax.set_ylim(top=2)
     np_image = mplfig_to_npimage(ax.figure)
     plt.close(ax.figure)
     return np_image
 
 clip = mpe.VideoClip(make_frame, duration=duration)
-small_clip = clip.crop(x1=60, x2=-60, y1=100, y2=-100).resize(0.5)
+small_clip = clip.crop(x1=60, y1=180, x2=-60, y2=-100)
 small_clip.write_gif("circular_animation.gif", fps=60)
